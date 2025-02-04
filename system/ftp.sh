db0dac6016fd9cc1e4563f9d57c3a9dcf5ab98b4
janis.roelli <janis.roelli@xplain.ch>
2025-02-04 12:55:20 +0100

    added fail2ban

--- system/ftp.sh
+++ system/ftp.sh
@@ -7,57 +7,49 @@ sudo chmod 755 /home/ftpusers
 
 # ================================================================================================
 
-# Add User for Minecraft Servers
-#                Home Directory                  Shell so User can't SSH   User Name
-sudo useradd -m -d /home/ftpusers/minecraft      -s /usr/sbin/nologin      minecraft 
-sudo useradd -m -d /home/ftpusers/satisfactory   -s /usr/sbin/nologin      satisfactory 
-sudo useradd -m -d /home/ftpusers/terraria       -s /usr/sbin/nologin      terraria 
+# Add User for Game Servers
+#                Home Directory          Shell so User can't SSH   User Name
+sudo useradd -m -d /home/ftpusers/games  -s /usr/sbin/nologin      games
 
 # Set Passwords
-sudo passwd minecraft
-sudo passwd satisfactory
-sudo passwd terraria
+sudo passwd games
 
 # ================================================================================================
 
 # Create a Directory for Game Server and bind it to the real one
-sudo mkdir /home/ftpusers/minecraft/vanilla
-sudo mkdir /home/ftpusers/minecraft/atm9
-sudo mkdir /home/ftpusers/minecraft/prominence
-sudo mkdir /home/ftpusers/minecraft/waschbecken
-
-sudo mkdir /home/ftpusers/satisfactory/chill
-sudo mkdir /home/ftpusers/satisfactory/sweat
-sudo mkdir /home/ftpusers/satisfactory/extra
-
-sudo mkdir /home/ftpusers/terraria/terraria-nuke
+sudo mkdir -p /home/ftpusers/games/minecraft/vanilla
+sudo mkdir -p /home/ftpusers/games/minecraft/atm9
+sudo mkdir -p /home/ftpusers/games/minecraft/prominence
+sudo mkdir -p /home/ftpusers/games/minecraft/waschbecken
+sudo mkdir -p /home/ftpusers/games/satisfactory/chill
+sudo mkdir -p /home/ftpusers/games/satisfactory/sweat
+sudo mkdir -p /home/ftpusers/games/satisfactory/extra
+sudo mkdir -p /home/ftpusers/games/terraria/terraria-nuke
 
 # ================================================================================================
 
 # OPTION 1: Temporary Bind Mounts
 #                 Host Source                       FTP Directory
-sudo mount --bind /docker/games/vanilla             /home/ftpusers/minecraft/vanilla
-sudo mount --bind /docker/games/atm9                /home/ftpusers/minecraft/atm9
-sudo mount --bind /docker/games/prominence          /home/ftpusers/minecraft/prominence
-sudo mount --bind /docker/games/waschbecken         /home/ftpusers/minecraft/waschbecken
-
-sudo mount --bind /docker/games/satisfactory-chill  /home/ftpusers/satisfactory/chill
-sudo mount --bind /docker/games/satisfactory-sweat  /home/ftpusers/satisfactory/sweat
-sudo mount --bind /docker/games/satisfactory-3      /home/ftpusers/satisfactory/extra
-
-sudo mount --bind /docker/games/terraria-nuke       /home/ftpusers/terraria/terraria-nuke
+sudo mount --bind /docker/games/vanilla             sudo mkdir /home/ftpusers/games/minecraft/vanilla
+sudo mount --bind /docker/games/atm9                sudo mkdir /home/ftpusers/games/minecraft/atm9
+sudo mount --bind /docker/games/prominence          sudo mkdir /home/ftpusers/games/minecraft/prominence
+sudo mount --bind /docker/games/waschbecken         sudo mkdir /home/ftpusers/games/minecraft/waschbecken
+sudo mount --bind /docker/games/satisfactory-chill  sudo mkdir /home/ftpusers/games/satisfactory/chill
+sudo mount --bind /docker/games/satisfactory-sweat  sudo mkdir /home/ftpusers/games/satisfactory/sweat
+sudo mount --bind /docker/games/satisfactory-3      sudo mkdir /home/ftpusers/games/satisfactory/extra
+sudo mount --bind /docker/games/terraria-nuke       sudo mkdir /home/ftpusers/games/terraria/terraria-nuke
 
 
 # OPTION 2: Permanent File Mounts in /etc/fstab
-#<file system>                             <mount point>                         <type>  <options>           <dump><pass>
-/docker/games/vanilla                       /home/ftpusers/minecraft/vanilla      none    bind                0     0
-/docker/games/atm9                          /home/ftpusers/minecraft/atm9         none    bind                0     0
-/docker/games/prominence                    /home/ftpusers/minecraft/prominence   none    bind                0     0
-/docker/games/waschbecken                   /home/ftpusers/minecraft/waschbecken  none    bind                0     0
-/docker/games/satisfactory-chill            /home/ftpusers/satisfactory/chill     none    bind                0     0
-/docker/games/satisfactory-sweat            /home/ftpusers/satisfactory/sweat     none    bind                0     0
-/docker/games/satisfactory-3                /home/ftpusers/satisfactory/extra     none    bind                0     0
-/docker/games/terraria-nuke                 /home/ftpusers/terraria/terraria-nuke none    bind                0     0
+#<file system>                             <mount point>                               <type>  <options>           <dump><pass>
+/docker/games/vanilla                       /home/ftpusers/games/minecraft/vanilla      none    bind                0     0
+/docker/games/atm9                          /home/ftpusers/games/minecraft/atm9         none    bind                0     0
+/docker/games/prominence                    /home/ftpusers/games/minecraft/prominence   none    bind                0     0
+/docker/games/waschbecken                   /home/ftpusers/games/minecraft/waschbecken  none    bind                0     0
+/docker/games/satisfactory-chill            /home/ftpusers/games/satisfactory/chill     none    bind                0     0
+/docker/games/satisfactory-sweat            /home/ftpusers/games/satisfactory/sweat     none    bind                0     0
+/docker/games/satisfactory-3                /home/ftpusers/games/satisfactory/extra     none    bind                0     0
+/docker/games/terraria-nuke                 /home/ftpusers/games/terraria/terraria-nuke none    bind                0     0
 
 # ================================================================================================
 
@@ -79,3 +71,39 @@ sudo systemctl enable vsftpd
 # Open Ports
 sudo ufw allow 21/tcp
 sudo ufw allow 40000:50000/tcp
+
+# ================================================================================================
+
+# Banlist
+sudo apt update && sudo apt install fail2ban -y
+
+
+# Jail Configuration
+sudo nano /etc/fail2ban/jail.local
+
+[vsftpd]
+enabled = true
+port = ftp,ftp-data,ftps
+filter = vsftpd
+logpath = /var/log/vsftpd.log
+maxretry = 3
+bantime = 31536000
+findtime = 600
+
+
+# Fail2Ban Filter
+sudo nano /etc/fail2ban/filter.d/vsftpd.conf
+
+[Definition]
+failregex = .*vsftpd.*FAILED LOGIN: Client "<HOST>".*
+ignoreregex =
+
+
+# Apply Changes
+sudo systemctl restart fail2ban
+sudo systemctl enable fail2ban
+
+
+# Check
+sudo fail2ban-client status vsftpd
+
